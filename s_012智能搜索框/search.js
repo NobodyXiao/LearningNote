c133f14645663de7d9e3664c3512fac7669c8f82
Xiaoxiao.Feng <haifxx@163.com>
2016-09-05 23:24:22 +0800

    change search box logic; add show/hide office-list

--- s_012智能搜索框/search.js
+++ s_012智能搜索框/search.js
@@ -7,35 +7,58 @@ $(document).ready(function(){
 var initialization=function(){
 //展示搜索的下拉框	
 	$(".edit").bind("keyup",function(){
-		$("#Drop-down").show();
+		$("#search-result-list").show();
 		$(document).bind('click', function() {  
-        $('#Drop-down').hide(); 
+        $('#search-result-list').hide(); 
     });	
 	})
-	tagle();
+    tagle();
 	shade();
 	login();
 	showIcon();
-	showselect();
 }
 //hover Office Online时候出现下拉框
-var tagle=function(){
-	 function stopPropagation(e) {  
+var tagle = function(){
+    var timeout_id;
+	function stopPropagation(e) {  
         if (e.stopPropagation)  
             e.stopPropagation();  
         else  
             e.cancelBubble = true;  
-    } 
-    $(document).bind("mouseout",function(){
-    	$('#menu-box').hide(); 	
-    })
-    $("#first-menu").bind("mouseenter",function(){
-    	$('#menu-box').show();
-    	stopPropagation(e);
-    })
+    }
+    // show logic 
+    $('#office-menu').mouseenter(function () {
+        clearTimeout(timeout_id);
+        $('#office-list').show(50);
+        stopPropagation(e); 
+    });
+    $('#office-list').mouseenter(function(event) {
+        clearTimeout(timeout_id);
+    });
 
+    // hide logic
+    $('#office-menu').mouseleave(function () {
+        timeout_id = setTimeout(function () {
+            if ($('#office-list').is(':visible')) { //if unvisible show up it
+                $('#office-list').hide();
+            }
+        }, 100);
+    });
+
+    $('#office-list').bind('mouseleave', function(event) {
+        timeout_id = setTimeout(function () {
+            if ($('#office-list').is(':visible')) { //if unvisible show up it
+                $('#office-list').hide();
+            }
+        }, 80);
+    });
+    $(document).bind("click",function(){ // hide it when click documents
+        if ($('#office-list').is(':visible')) {
+    	   $('#office-list').hide();
+        }
+    })
 }
-//为背景图改变透明度，来获得添加遮罩层的效果
+//为背景图改变透明度，来获得添加遮罩层的效果,单击搜索结果，输入框内容会更改并跳转
 var shade=function(){
  
   	 function stopPropagation(e) {  
@@ -49,10 +72,19 @@ var shade=function(){
     });  
   
     $('.edit').bind('click', function(e) {  
-         $('.bground').css('opacity', '0.6');
+        $('.bground').css('opacity', '0.6');
         stopPropagation(e);  
     });
+    //当鼠标点击搜索结果的时候，输入框的内容变成点击的文字内容
+    var result = document.getElementById("search-result");
+    var resultList = result.getElementsByTagName("li");
+    $.each(resultList,function(){
+        $(this).bind("click",function(){
+            $(".edit").val($(this).text());
+        })
+    })
     }
+
 //点击登录按钮，出现登录下拉框，实现效果
     var login=function(){
     	var flag=0;
@@ -69,7 +101,7 @@ var shade=function(){
         flag=0; 
     });  
   
-    $('.checkin').bind('click', function(e) {  
+    $('.login').bind('click', function(e) {  
     	
     	if(flag==0){
 	         $('#login-box').show();
@@ -95,7 +127,7 @@ var shade=function(){
         else  
             e.cancelBubble = true;  
     } 
-    	$("#share").bind("mouseenter",function(){
+    	$("#share").bind("mouseenter",function(e){
     		$("#QQ").show(250);
     		$("#weixin").show(150);
     		$("#weibo").show(50);
@@ -108,22 +140,4 @@ var shade=function(){
     		$("#weibo").hide(50);
     	})
     }
-    var showselect = function(){
-   			 
-   		function stopPropagation(e) 
-		    {  
-		        if (e.stopPropagation)  
-		            e.stopPropagation();  
-		        else  
-		            e.cancelBubble = true;  
-		    } 
-    	$("#tab").bind("mouseenter",function(){
-    		$("#tab-2").show();
-
-    	})
-    	
-    	$(document).bind("click",function(){
-    		$("#tab-2").hide();
-    	})
-
-    }
+  
