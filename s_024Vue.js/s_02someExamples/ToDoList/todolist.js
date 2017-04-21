d9b4626efc6064f7ce48a44922c48ab25cc78f74
NobodyXiao <haifxx@163.com>
2017-04-21 08:52:02 +0800

    start todolist

---
+++ s_024Vue.js/s_02someExamples/ToDoList/todolist.js
@@ -0,0 +1,16 @@
+var demo = new Vue({
+	el:'#app',
+	data:{
+		items:[
+			{
+				label:'finish my job',
+				isFinished:false
+			},
+			{
+				label:'watch a movie',
+				isFinished:true
+			}
+
+		]
+	}
+})
