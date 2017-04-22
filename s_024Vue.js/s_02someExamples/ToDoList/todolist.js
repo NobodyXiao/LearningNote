31d080caac71a432346f7524ca214fd3ae6659eb
NobodyXiao <haifxx@163.com>
2017-04-22 22:36:24 +0800

    finish the todolist

--- s_024Vue.js/s_02someExamples/ToDoList/todolist.js
+++ s_024Vue.js/s_02someExamples/ToDoList/todolist.js
@@ -1,16 +1,38 @@
+// import Store from './store';	
+const STORAGE_KEY = '';
+ Store={
+	fetch : function(){
+		return JSON.parse(window.localStorage.getItem(STORAGE_KEY) || '[]')
+	},
+	save : function(items){
+		window.localStorage.setItem(STORAGE_KEY,JSON.stringify(items))
+	}
+};
 var demo = new Vue({
 	el:'#app',
 	data:{
-		items:[
-			{
-				label:'finish my job',
-				isFinished:false
+		text:'',
+		items:Store.fetch(),
+			
+	},
+	methods:{
+			changeClass:function(item){
+				item.isFinished = !item.isFinished;
 			},
-			{
-				label:'watch a movie',
-				isFinished:true
+			addThings:function(){
+				this.items.push({label:this.text,isFinished:false});
+				this.text='';	
+			},
+			deleteThing:function(item){
+				item.isDelete = true;
 			}
-
-		]
+		},
+	watch:{
+			items:{
+				handler:function(items){
+					Store.save(items);
+				},
+				deep:true
+		}
 	}
 })
